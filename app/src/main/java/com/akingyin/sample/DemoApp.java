3e70e64b21f764e6ea6e381730bb9f7d65bebcf5
akingyin1987 <akingyin@163.com>
2018-03-15 17:47:27 +0800

    Initial commit

---
+++ app/src/main/java/com/akingyin/sample/DemoApp.java
@@ -0,0 +1,11 @@
+package com.akingyin.sample;
+
+/**
+ * @author king
+ * @version V1.0
+ * @ Description:
+ * @ Date 2018/3/15 17:43
+ */
+
+public class DemoApp {
+}
---
+++ app/src/main/java/com/akingyin/sample/db/UserEntity.java
@@ -0,0 +1,14 @@
+package com.akingyin.sample.db;
+
+import java.io.Serializable;
+
+/**
+ * @author king
+ * @version V1.0
+ * @ Description:
+ * @ Date 2018/3/15 17:44
+ */
+
+
+public class UserEntity  implements Serializable {
+}
